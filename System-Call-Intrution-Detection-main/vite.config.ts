82efc0c7f615fcba2c1c0b0ba426e1eb898f5b42
Varsh24-hash <varshinisankar24@gmail.com>
2026-01-16 18:32:35 +0530

    Update vite.config.ts

--- System-Call-Intrution-Detection-main/vite.config.ts
+++ System-Call-Intrution-Detection-main/vite.config.ts
@@ -2,7 +2,8 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ plugins: [react()],
+  base: '/',  }) => {
     const env = loadEnv(mode, '.', '');
     return {
       server: {
